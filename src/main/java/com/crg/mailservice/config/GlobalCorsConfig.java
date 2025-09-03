dd4f1c00bf30bc98c547c99048211ff8e726e1c2
Chittaranjan Ghosh <chittaranjan@guvi.in>
2025-09-03 16:05:16 +0530

    Update GlobalCorsConfig.java

--- src/main/java/com/crg/mailservice/config/GlobalCorsConfig.java
+++ src/main/java/com/crg/mailservice/config/GlobalCorsConfig.java
@@ -14,7 +14,7 @@ public class GlobalCorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000")
+                        .allowedOrigins("https://campaignbuddy.netlify.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
